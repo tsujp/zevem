cacc901d5b68941d9172ee9836a56730f02c13f6
Jordan Ellis Coppard <jc+git@wz.ht>
2025-01-25 22:42:20 +0900

    tidy build.zig; add zig version check
    - If you hate the version check in build.zig just comment it out.

--- build.zig
+++ build.zig
@@ -1,18 +1,7 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
-// Foo
-
-// @import("foo") -- imports _module_ foo.
-// packages are pieces of code downloaded and/or depended on in the Zig package manager
-// libraries are explicitly C-style static or dynamic libraries (.so, .dll) etc
-
-// `build` declaratively constructs a build graph to be executed by an external
-//   runner (e.g. the `zig build` command).
 pub fn build(b: *std.Build) void {
-    // `standardTargetOptions` et al narrow the targets and optimisations the
-    //   runner can provide, these particular ones do not narrow at all and so
-    //   allow all the defaults. If we only wanted to allow building WASM we
-    //   could define and then set them in-place of `standardTargetOptions`.
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -23,34 +12,31 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    // TODO: `b.installArtifact(bin)` step when this is ready for distribution.
-
-    // Creates a run step. This can be executed when another step is evaluated
-    //   that depends on it; done-so by exposing it as a build step (visible in
-    //   `zig build --help`) using `b.step()`.
     const run_cmd = b.addRunArtifact(bin);
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // Creates a build step (visible in `zig build --help`) and declares a
-    //   dependency on the `run_cmd` run step. Now executing `zig build run`
-    //   allows us to build and execute `bin`.
     const run_step = b.step("run", "Do the thing");
     run_step.dependOn(&run_cmd.step);
 
-    // Creates a test step which will build the test executable (but not run it).
     const unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // Creates a run step for our test executable.
     const run_unit_tests_cmd = b.addRunArtifact(unit_tests);
 
-    // Crates a build step for unit tests executable.
     const test_step = b.step("test", "Test the thing");
     test_step.dependOn(&run_unit_tests_cmd.step);
+}
+
+comptime {
+    const zig_ver_str = "0.14.0-dev.2851+b074fb7dd";
+    const supported_zig = std.SemanticVersion.parse(zig_ver_str) catch unreachable;
+    if (builtin.zig_version.order(supported_zig) != .eq) {
+        @compileError(std.fmt.comptimePrint("Unsupported Zig version {}; require {s}", .{ builtin.zig_version, zig_ver_str }));
+    }
 }
