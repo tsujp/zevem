13fc608713e9ca6adc254ebf9f92769d68c996d4
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2025-02-04 12:08:41 +0100

    evm: add a basic execution environment
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
+const util = @import("util.zig");
 const print = std.debug.print;
 const EnumField = std.builtin.Type.EnumField;
 const fmt = std.fmt;
-
-const EVM = @import("evm.zig").EVM;
+const DummyEnv = util.DummyEnv;
+const EVM = util.EVM;
 
 pub fn main() !void {
-    var evm2 = try EVM.init();
+    var dummyEnv = DummyEnv{};
+    var evm2 = try EVM.init(&dummyEnv);
     try evm2.execute(&.{ 0x5F, 0x60, 0x11, 0x61, 0x22, 0x33, 0x00 });
 }
--- src/util.zig
+++ src/util.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
-const EVM = @import("evm.zig").EVM;
+const NewEVM = @import("evm.zig").NewEVM;
+
+pub const DummyEnv = struct {
+    pub fn getBalance(_: *DummyEnv, _: u256) !u256 {
+        return 0;
+    }
+};
+pub const EVM = NewEVM(DummyEnv);
 
 // TODO: Place elsewhere, idk.
 // Inspired by: https://github.com/jsign/phant/blob/18eeadffd24de9c6b3ae5c7505ada52c43f2b1d4/src/common/hexutils.zig#L73
@@ -12,7 +19,8 @@ pub inline fn htb(comptime bytes: []const u8) [bytes.len / 2]u8 {
 // TODO: Replace this with test files on-disk which are read to execute EVM instructions and then the unit test in Zig code is about asserting expected values; or do it all in-code but in any case abstract creation of EVM.
 // TODO: Probably also want some re-usable one to avoid re-allocating every single time in these unit tests? But fresh context is important, but also testing properly is important.
 pub fn evmBasicBytecode(comptime bytes: []const u8) !EVM {
-    var evm = try EVM.init();
+    var dummyEnv: DummyEnv = .{};
+    var evm = try EVM.init(&dummyEnv);
     try evm.execute(&htb(bytes));
 
     return evm;
