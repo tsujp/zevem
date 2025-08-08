3375a031342a3b19e80dc25b66ceeacb1827b7fc
Jordan Ellis Coppard <jc+git@wz.ht>
2025-08-08 18:48:48 +0900

    update standard types and BlockHeader types
    - The size of base_fee is still a bit iffy since it can overflow a u64 when
      denominated in Wei, but it will do for now. Worst-case can just use u256.

--- src/lib/types.zig
+++ src/lib/types.zig
@@ -4,8 +4,14 @@ pub const Word = u256;
 pub const DoubleWord = u512;
 pub const SignedWord = i256;
 
-// TODO: Or [20]u8?
-pub const Address = u160;
+// No maths done on addresses (until Verkle) so array of 20 bytes better.
+pub const Address = [20]u8;
+
+pub const Hash32 = [32]u8;
+pub const Bytes32 = [32]u8;
+
+// TODO: Comptime `Bytes` function which takes comptime in and returns an array
+//       of that size? so `Bytes(32)` for [32]u8 and `Bytes(20)` for [20]u8?
 
 /// Blocks.
 pub const BlockHeader = @import("types/BlockHeader.zig");
--- src/lib/types/BlockHeader.zig
+++ src/lib/types/BlockHeader.zig
@@ -5,33 +5,33 @@ const types = @import("../types.zig");
 const BlockHeader = @This();
 
 /// Current block's parent's header Keccak-256 hash: I_H_p
-parent_hash: u8, // TODO: Proper type.
+parent_hash: types.Hash32,
 
 /// Current block's beneficiary (coinbase) address: I_H_c
 beneficiary: types.Address,
 
 /// Current block's number: I_H_i
-number: u256,
+number: u64,
 
 /// Current block's gas limit: I_H_l
-gas_limit: u8, // TODO: Proper type.
+gas_limit: u64,
 
 /// Current block's timestamp: I_H_s
-timestamp: u8, // TODO: Proper type.
+timestamp: u64,
 
 /// Current block's RANDAO mix: I_H_a
-randao: u8, // TODO: Proper type.
+randao: types.Bytes32, // TODO: Bytes32 is the proper type right?
 
 /// Current block's base fee: I_H_f
-base_fee: u8, // TODO: Proper type.
+base_fee: u64, // TODO: Bigger type since denominated in Wei and can overflow a u64
 
 // Decl literals for easier initialisation.
 pub const default: BlockHeader = .{
     // TODO: Reasonable default values for this.
     .parent_hash = 1,
-    .beneficiary = 2,
+    .beneficiary = 0,
     .number = 0,
-    .gas_limit = 3,
+    .gas_limit = 30_000_000, // Arbitrary.
     .timestamp = 4,
     .randao = 5,
     .base_fee = 6,
